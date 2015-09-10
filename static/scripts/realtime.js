a516a1e48255eec53b64b14a95c0f1892ed203a4
Tim Ryan <tim@3drobotics.com>
2015-09-10 13:09:28 -0700

    Removes base64 encoder.

--- static/scripts/realtime.js
+++ static/scripts/realtime.js
@@ -27,34 +27,6 @@ var FUDGE = 0.0005;
 var OFFSETX = 0.0001;
 var OFFSETY = -0.0002;
 
-function base64Encode(str) {
-  var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  var out = "", i = 0, len = str.length, c1, c2, c3;
-  while (i < len) {
-      c1 = str.charCodeAt(i++) & 0xff;
-      if (i == len) {
-          out += CHARS.charAt(c1 >> 2);
-          out += CHARS.charAt((c1 & 0x3) << 4);
-          out += "==";
-          break;
-      }
-      c2 = str.charCodeAt(i++);
-      if (i == len) {
-          out += CHARS.charAt(c1 >> 2);
-          out += CHARS.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-          out += CHARS.charAt((c2 & 0xF) << 2);
-          out += "=";
-          break;
-      }
-      c3 = str.charCodeAt(i++);
-      out += CHARS.charAt(c1 >> 2);
-      out += CHARS.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
-      out += CHARS.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
-      out += CHARS.charAt(c3 & 0x3F);
-  }
-  return out;
-}
-
 function addImage(lat, lon, src) {
   var imageLayer = new ol.layer.Image({
     // opacity: 0.75,
