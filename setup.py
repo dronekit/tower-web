c1b01cf7d260e8af778b862ce495996d5545b234
Tim Ryan <tim@3drobotics.com>
2015-09-30 06:26:19 -0700

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='galacsian',
+      version='1.0.0',
+      description='GCS.',
+      author='Tim Ryan',
+      author_email='tim@3drobotics.com',
+      url='https://github.com/3drobotics/galacsian.git',
+      install_requires = [
+      	'Flask==0.10.1',
+      	'protobuf==3.0.0a1',
+		'requests==2.5.1',
+		'wheel==0.24.0'
+	  ],
+	  dependency_links = [
+		'git+https://github.com/dronekit/dronekit-python@v2.0.0-beta1',
+      ],
+      entry_points={
+          'console_scripts': [
+              'galacsian = galacsian'
+          ]
+      }
+      )
